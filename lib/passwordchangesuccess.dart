6cdb7aa0c3e0b6b24333bd05baf4b50c329f6246
aman <singhamish27@gmail.com>
2020-10-10 12:21:41 +0530

    upto password success

--- lib/passwordchangesuccess.dart
+++ lib/passwordchangesuccess.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 
 class PasswordChangeSuccess extends StatelessWidget {
   @override
@@ -7,7 +9,56 @@ class PasswordChangeSuccess extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Avenir',
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/success.png'),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Successful!',
+              style: TextStyle(
+                fontSize: 35,
+              ),
+            ),
+            Text(
+              'You have successfully changed your password. Please use your new password to Login',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                );
+              },
+              color: Colors.teal,
+              padding: EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+              child: Text(
+                'Continue',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
