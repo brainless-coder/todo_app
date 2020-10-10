be37ba941a6ceac2a81a0ba2b4cd01bfe5c06faa
aman <singhamish27@gmail.com>
2020-10-10 11:01:29 +0530

    completed forgot password page

--- lib/forgotpassword.dart
+++ lib/forgotpassword.dart
@@ -3,6 +3,85 @@ import 'package:flutter/material.dart';
 class ForgotPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Avenir',
+      ),
+      home: GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: ListView(
+              children: [
+                Text(
+                  'Forgot Password',
+                  style: TextStyle(
+                    fontSize: 36,
+                  ),
+                ),
+                Text(
+                  'Please enter your mail ID below to receive password reset information',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      labelText: 'Mail ID',
+                      hintText: 'hello@example.com',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  color: Colors.redAccent,
+                  onPressed: () {},
+                  child: Text(
+                    'Send Request',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
