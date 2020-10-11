47a4d5c86dd76c083a1cc60e9cde5fee2e136acc
aman <singhamish27@gmail.com>
2020-10-11 19:43:20 +0530

    completed upto slideable card

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,8 +1,256 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Avenir',
+      ),
+      home: HomePageBody(),
+    );
+  }
+}
+
+class HomePageBody extends StatefulWidget {
+  @override
+  _HomePageBodyState createState() => _HomePageBodyState();
+}
+
+class _HomePageBodyState extends State<HomePageBody> {
+  String filterType = "today";
+  CalendarController _calendarController = new CalendarController();
+  DateTime today = DateTime.now();
+  var monthNames = [
+    'JAN',
+    'FEB',
+    'MAR',
+    'APR',
+    'MAY',
+    'JUN',
+    'JUL',
+    'AUG',
+    'SEP',
+    'OCT',
+    'NOV',
+    'DEC',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFF96060),
+        elevation: 0,
+        title: Text(
+          'Work List',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.short_text,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 70,
+                color: Color(0xfff96060),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              filterType = "today";
+                            });
+                          },
+                          child: Text(
+                            'Today',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: 4,
+                          width: 120,
+                          color: filterType == "today"
+                              ? Colors.white
+                              : Colors.transparent,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              filterType = "monthly";
+                            });
+                          },
+                          child: Text(
+                            'Monthly',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: 4,
+                          width: 120,
+                          color: filterType == "monthly"
+                              ? Colors.white
+                              : Colors.transparent,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              (filterType == "monthly")
+                  ? TableCalendar(
+                      calendarController: _calendarController,
+                      initialCalendarFormat: CalendarFormat.week,
+                    )
+                  : Container(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Today ${monthNames[today.month - 1]} ${today.day} / ${today.year}",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      taskWidget(Color(0xfff96060), "Going out", "10:00 AM"),
+                      taskWidget(Colors.teal, "Going out", "10:00 AM"),
+                      taskWidget(Colors.blueAccent, "Going out", "10:00 AM"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Slidable taskWidget(Color color, String title, String time) {
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.3,
+      child: Container(
+        height: 80,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.03),
+              offset: Offset(0, 9),
+              blurRadius: 20,
+              spreadRadius: 2,
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              height: 25,
+              width: 25,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: color,
+                  width: 4,
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  time,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+            Expanded(
+              child: Container(),
+            ),
+            Container(
+              height: 70,
+              width: 7,
+              color: color,
+            ),
+          ],
+        ),
+      ),
+      secondaryActions: [
+        IconSlideAction(
+          onTap: () {},
+          caption: "Edit",
+          color: Colors.white,
+          icon: Icons.edit,
+          foregroundColor: Colors.blueAccent,
+        ),
+        IconSlideAction(
+          onTap: () {},
+          caption: 'Delete',
+          icon: Icons.delete,
+          foregroundColor: Colors.red,
+        )
+      ],
+    );
   }
 }
